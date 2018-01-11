58c9ef8e169dfb2fc4d13828e664a28e64d6a1ee
chendotjs <chendotjs@gmail.com>
2018-01-11 20:01:29 +0800

    add IN/OUT operation on connection_t

--- src/lotos_epoll.c
+++ src/lotos_epoll.c
@@ -7,22 +7,22 @@ struct epoll_event lotos_events[MAX_EVENTS];
 
 int lotos_epoll_create(int flags) { return epoll_create1(flags); }
 
-int lotos_epoll_add(int epoll_fd, connection_t *pconn, uint32_t events,
+int lotos_epoll_add(int epoll_fd, connection_t *c, uint32_t events,
                     struct epoll_event *pev) {
-  FILL_EPOLL_EVENT(pev, pconn, events);
-  return epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pconn->fd, pev);
+  FILL_EPOLL_EVENT(pev, c, events);
+  return epoll_ctl(epoll_fd, EPOLL_CTL_ADD, c->fd, pev);
 }
 
-int lotos_epoll_mod(int epoll_fd, connection_t *pconn, uint32_t events,
+int lotos_epoll_mod(int epoll_fd, connection_t *c, uint32_t events,
                     struct epoll_event *pev) {
-  FILL_EPOLL_EVENT(pev, pconn, events);
-  return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, pconn->fd, pev);
+  FILL_EPOLL_EVENT(pev, c, events);
+  return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, c->fd, pev);
 }
 
-int lotos_epoll_del(int epoll_fd, connection_t *pconn, uint32_t events,
+int lotos_epoll_del(int epoll_fd, connection_t *c, uint32_t events,
                     struct epoll_event *pev) {
   (void)pev; // make compiler happy
-  return epoll_ctl(epoll_fd, EPOLL_CTL_DEL, pconn->fd, NULL);
+  return epoll_ctl(epoll_fd, EPOLL_CTL_DEL, c->fd, NULL);
 }
 
 inline int lotos_epoll_wait(int epoll_fd, struct epoll_event *events,
--- src/lotos_epoll.h
+++ src/lotos_epoll.h
@@ -1,6 +1,7 @@
 #ifndef _LOTOS_EPOLL_H__
 #define _LOTOS_EPOLL_H__
 #include "connection.h"
+#include "misc.h"
 #include <sys/epoll.h>
 
 #define MAX_EVENTS (10240)
@@ -10,17 +11,47 @@
     ev->data.ptr = pconn;                                                      \
     ev->events = e_events;                                                     \
   } while (0)
+#define CONN_IS_IN(c) ((c)->event.events & EPOLLIN)
+#define CONN_IS_OUT(c) ((c)->event.events & EPOLLOUT)
 
 extern struct epoll_event lotos_events[MAX_EVENTS]; // global
 
 extern int lotos_epoll_create(int flags);
-extern int lotos_epoll_add(int epoll_fd, connection_t *restrict pconn,
+extern int lotos_epoll_add(int epoll_fd, connection_t *restrict c,
                            uint32_t events, struct epoll_event *pev);
-extern int lotos_epoll_mod(int epoll_fd, connection_t *restrict pconn,
+extern int lotos_epoll_mod(int epoll_fd, connection_t *restrict c,
                            uint32_t events, struct epoll_event *pev);
-extern int lotos_epoll_del(int epoll_fd, connection_t *restrict pconn,
+extern int lotos_epoll_del(int epoll_fd, connection_t *restrict c,
                            uint32_t events, struct epoll_event *pev);
 extern int lotos_epoll_wait(int epoll_fd, struct epoll_event *events,
                             int max_events, int timeout);
+
+static inline int connection_enable_in(int epoll_fd, connection_t *c) {
+  if (CONN_IS_IN(c))
+    return OK;
+  c->event.events |= EPOLLIN;
+  return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, c->fd, &c->event);
+}
+
+static inline int connection_disable_in(int epoll_fd, connection_t *c) {
+  if (!CONN_IS_IN(c))
+    return OK;
+  c->event.events &= ~EPOLLIN;
+  return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, c->fd, &c->event);
+}
+
+static inline int connection_enable_out(int epoll_fd, connection_t *c) {
+  if (CONN_IS_OUT(c))
+    return OK;
+  c->event.events |= EPOLLOUT;
+  return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, c->fd, &c->event);
+}
+
+static inline int connection_disable_out(int epoll_fd, connection_t *c) {
+  if (!CONN_IS_OUT(c))
+    return OK;
+  c->event.events &= ~EPOLLOUT;
+  return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, c->fd, &c->event);
+}
 
 #endif
